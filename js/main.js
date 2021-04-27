3b7e82e438d2d648b315a12cf8b3397b3b3171cb
planeset94 <planeset94@gmail.com>
2021-04-27 11:54:09 +0200

    problemi con il cambio di variabile

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
-// L’utente inserisce due parole in successione, con due prompt.
-var parola_1= prompt('Insierisci una parola');
-var parola_2= prompt('Insierisci un\'altra parola');
+// // L’utente inserisce due parole in successione, con due prompt.
+// var parola_1= prompt('Insierisci una parola');
+// var parola_2= prompt('Insierisci un\'altra parola');
 
-console.log(parola_1);
-console.log(parola_2);
+// console.log(parola_1);
+// console.log(parola_2);
 
-// Il software stampa prima la parola più corta, poi la parola più lunga.
-var lunghezza_1;
-var lunghezza_2;
+// // Il software stampa prima la parola più corta, poi la parola più lunga.
+// var lunghezza_1;
+// var lunghezza_2;
 
-lunghezza_1=parola_1.length;
-lunghezza_2=parola_2.length;
-
-
+// lunghezza_1=parola_1.length;
+// lunghezza_2=parola_2.length;
 
 
-switch (true){
-    case lunghezza_1 < lunghezza_2:
-        console.log('prima parola: ' +lunghezza_1);
-        console.log('seconda parola: ' +lunghezza_2);
 
-    break;
 
-    case lunghezza_1 > lunghezza_2:
-        console.log('seconda parola: ' +lunghezza_2);
-        console.log('prima parola: ' +lunghezza_1);
+// switch (true){
+//     case lunghezza_1 < lunghezza_2:
+//         console.log('prima parola: ' +lunghezza_1);
+//         console.log('seconda parola: ' +lunghezza_2);
 
-    break;
+//     break;
+
+//     case lunghezza_1 > lunghezza_2:
+//         console.log('seconda parola: ' +lunghezza_2);
+//         console.log('prima parola: ' +lunghezza_1);
+
+//     break;
+// }
+
+// Snack 3:
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+var numero;
+
+for (var i=0; i<10; i++){
+       numero=Number(prompt('Insert a number'));
+       numero = numero *i;
 }
 
+console.log(numero);
+
+
+
+
+
+
+
+
+
+// Il programma stampa la somma di tutti i numeri inseriti. 
