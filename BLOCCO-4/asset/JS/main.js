f94535f937865c4e6e5a8aa89d1dbf2cd2306402
planeset94 <planeset94@gmail.com>
2021-05-10 11:45:14 +0200

    es 1B: change weight

--- BLOCCO-4/asset/JS/main.js
+++ BLOCCO-4/asset/JS/main.js
@@ -1,14 +1,23 @@
+
+
+// 1A
 // Da mettere in una sottocartella nella repo della volta scorsa js-jsnacks-blocco-1
 // 1a:
 // Creare un oggetto palla che abbia le seguenti proprietà.
 // Nome = palla
 // Peso = 10
 
-// 1A
 
-var palla = {
+var Palla = {
     nome: 'palla',
     peso: 10,
 };
 
-console.log(palla);
+console.log(Palla);
+
+
+//1B
+// Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+userWeight = Number(prompt('Scrivi il peso della palla'));
+Palla.peso = userWeight;
+console.log(Palla);
