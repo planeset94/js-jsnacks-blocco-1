3fdf9ff34ccf69006803bd72ad1f5e93e9744533
planeset94 <planeset94@gmail.com>
2021-05-10 11:39:29 +0200

    BLOCCO4-es1

---
+++ BLOCCO-4/asset/JS/main.js
@@ -0,0 +1,14 @@
+// Da mettere in una sottocartella nella repo della volta scorsa js-jsnacks-blocco-1
+// 1a:
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+
+// 1A
+
+var palla = {
+    nome: 'palla',
+    peso: 10,
+};
+
+console.log(palla);
